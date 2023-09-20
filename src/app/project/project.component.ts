1c0ca39be72520fd8b8cd3d285e1dfc6711f575a
Alen Alduk <youstylez@gmail.com>
2023-09-20 16:48:01 +0200

    changes on animations

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import * as AOS from 'aos'
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent {
-
+  ngOnInit(){
+    AOS.init();
+  }
 }
