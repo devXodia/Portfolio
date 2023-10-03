82d2983232719352f12ddb04dbc9c11a425a029e
Alen Alduk <youstylez@gmail.com>
2023-10-03 10:52:28 +0200

    changed responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { MainScreenComponent } from './main-screen/main-screen.component';
-import { LinksSectionComponent } from './links-section/links-section.component';
+
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { BetweenPsComponent } from './between-ps/between-ps.component';
@@ -14,15 +14,13 @@ import { ContactSectionComponent } from './contact-section/contact-section.compo
 import { BetweenCfComponent } from './between-cf/between-cf.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { MainContentComponent } from './main-content/main-content.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     MainScreenComponent,
-    LinksSectionComponent,
     AboutMeComponent,
     SkillsComponent,
     BetweenPsComponent,
@@ -32,15 +30,9 @@ import { MainContentComponent } from './main-content/main-content.component';
     FooterComponent,
     ImpressumComponent,
     MainContentComponent,
-    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -1,17 +1,18 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import * as AOS from 'aos'
+import * as AOS from 'aos';
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
-  styleUrls: ['./main-screen.component.scss']
+  styleUrls: ['./main-screen.component.scss'],
 })
-
-
-
 export class MainScreenComponent {
-
-  ngOnInit(){
+  ngOnInit() {
     AOS.init();
-  } 
+  }
 
+  showModal = false;
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
 }
